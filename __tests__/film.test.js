56530b2572ad6a45fb2466ae4ea9dbd1389d0106
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-25 16:56:18 -0700

    GREEN - Get all films with studio

--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -4,6 +4,7 @@ import db from '../lib/utils/db.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Studio from '../lib/models/Studio.js';
+import Film from '../lib/models/Film.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -35,10 +36,55 @@ describe('demo routes', () => {
       createdAt: expect.any(String)
     });
   });
+
+  it('GETs all films with studio, studio id & studio name', async () => {
+    const studio1 = await Studio.create({
+      name: 'MGM',
+      city: 'Los Angeles',
+      state: 'California',
+      country: 'USA'
+    });
+
+    const studio2 = await Studio.create({
+      name: 'MGV',
+      city: 'Lost',
+      state: 'Oregon',
+      country: 'USA'
+    });
+
+    await Film.create({
+      title: 'Fast & Furious',
+      StudioId: studio1.id,
+      released: 2017,
+    });
+
+    await Film.create({
+      title: 'Slow & Furious',
+      StudioId: studio2.id,
+      released: 2017,
+    });
+
+    const res = await request(app).get('/api/v1/films/');
+
+    expect(res.body).toEqual([
+      {
+        id: 1, title: 'Fast & Furious',
+        released: 2017,
+        Studio: { id: studio1.id, name: studio1.name },
+        StudioId: 1,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      },
+      {
+        id: 2, title: 'Slow & Furious',
+        released: 2017,
+        Studio: { id: studio2.id, name: studio2.name },
+        StudioId: 2,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      }
+    ],
+    );
+  });
 });
 
-// {
-//   title: <title of film RS>,
-//   studio: <studio id RI>,
-//   released: <4-digit year RN>
-// }
