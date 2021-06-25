d31463e3fd05dc647e31abefb0aa086c019b95e8
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-25 09:56:51 -0700

    Reviewer - RED test

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -8,20 +8,18 @@ describe('demo routes', () => {
     return db.sync({ force: true });
   });
 
-  it('POSTS an actor', async () => {
+  it('POSTS an reviewer', async () => {
     const res = await request(app)
-      .post('/api/v1/actors')
+      .post('/api/v1/reviewer')
       .send({
-        name: 'Brad Pitt',
-        dob: new Date(1963, 12, 18),
-        pob: 'Shawnee, Oklahoma'
+        name: 'Roger Ebert',
+        company: 'Skiskel & Ebert'
       });
       
     expect(res.body).toEqual({
       id: 1,
-      name: 'Brad Pitt',
-      dob: new Date(1963, 12, 18).toISOString(),
-      pob: 'Shawnee, Oklahoma',
+      name: 'Roger Ebert',
+      company: 'Skiskel & Ebert',
       updatedAt: expect.any(String),
       createdAt: expect.any(String),
     });
