fbd41bd6bed9b4e7280ad7ab19cf93ae2bb55bd6
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-06-24 17:57:40 -0700

    film test and controller

---
+++ __tests__/film.test.js
@@ -0,0 +1,30 @@
+
+import db from '../lib/utils/db.js';
+// import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  it('POSTS a film', async () => {
+    const res = await request(app)
+      .post('/api/v1/films')
+      .send({
+        title: 'Fast & Furious',
+        studio: 'MGM',
+        released: 2017,
+      });
+
+    expect(res.body).toEqual({
+      id: 1,
+      title: 'Fast & Furious',
+      studio: 'MGM',
+      released: 2017,
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+});
