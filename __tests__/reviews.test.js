9e0e332dc2d27ff4b2d4f4cf0e018747e1bc4847
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-27 15:07:28 -0700

    RED Review Test and model

---
+++ __tests__/reviews.test.js
@@ -0,0 +1,54 @@
+import db from '../lib/utils/db.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Film from '../lib/models/Film.js';
+import Review from '../lib/models/Review.js';
+import Reviewer from '../lib/models/Reviewer.js';
+import Studio from '../lib/models/Studio.js';
+
+describe('demo routes', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  it('GET all reviews', async() => {
+    const studio = await Studio.create({
+      name: 'MGM',
+      city: 'Los Angeles',
+      state: 'California',
+      country: 'USA'
+    });
+
+    const film = await Film.create({
+      title: 'Terminator',
+      StudioId: studio.id,
+      released: 1993,
+    });
+
+    const reviewer = await Reviewer.create({
+      name: 'Roger Ebert',
+      company: 'Siskel & Ebert'
+    });
+    
+    const review = await Review.create({
+      rating: 4,
+      reviewer: reviewer.name,
+      review: 'Terminator is good!',
+    });
+
+    await review.addFilm(film);
+    
+    const res = await request(app)
+      .get('/api/v1/reviews');
+
+    console.log('$$$$', res.body);
+
+    expect(res.body).toEqual([{
+      id: 1, rating: 4, review: 'Terminator is good!',
+      film: [{ id: 1, title: 'Terminator' }],
+      films: null,
+      updatedAt: studio.updatedAt.toISOString(),
+      createdAt: studio.createdAt.toISOString()
+    }]);
+  });
+});
